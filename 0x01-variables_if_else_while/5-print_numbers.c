844dec547e4ba3c36ebe44523ce42b425163acff
Pinnacle96 <binary.rln@gmail.com>
2022-09-09 03:51:22 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end of the program
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; ++n)
+		printf("%d", n);
+
+	printf("\n");
+	return (0);
+}
