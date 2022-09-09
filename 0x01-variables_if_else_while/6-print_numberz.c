f7f8a41cef3dd846ff7050e06b27e59cf8859fd8
Pinnacle96 <binary.rln@gmail.com>
2022-09-09 03:52:58 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end of the program
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; ++n)
+		putchar(n);
+
+	putchar(10);
+
+	return (0);
+}
