3925e67d0c6dc9b823ab6b4660af5c7e05ad8314
Pinnacle96 <binary.rln@gmail.com>
2022-10-31 08:57:36 -0700

    edit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,114 +1,72 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * close_file - close file and check if was succesfull
- * @fd: file descriptor
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-void close_file(int fd)
+void error_file(int file_from, int file_to, char *argv[])
 {
-	if (close(fd) == -1)
+	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-}
-
-/**
- * error_handler - returns respective exit status
- * @fd: file descriptor
- * @fname: file descriptor
- * @type: type of sys call
- * @buffer: buffer to be free if exit status
- * Return: 0 on success, exit status on error
- */
-int error_handler(int fd, char *fname, int type, char *buffer)
-{
-	int status = 0;
-	(void)fd;
-
-	switch (type)
+	if (file_to == -1)
 	{
-	case READ_ERROR:
-		dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n", fname);
-		if (buffer != NULL)
-			free(buffer);
-		status = 98;
-		break;
-	case WRITE_ERROR:
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", fname);
-		if (buffer != NULL)
-			free(buffer);
-		status = 99;
-		break;
-	default:
-		break;
-	}
-	return (status);
-}
-
-/**
- * create_buffer - allocates a buffer of BUFFERSIZE
- * @fileName: file dest to print error
- * Return: pointer to new allocated buffer
- */
-char *create_buffer(char *fileName)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * BUFFERSIZE);
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fileName);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	return (buffer);
 }
 
 /**
- * main - copies contents of 1 file to another
- * @argc: number of parameters
- * @argv: array of string parameters
- * Return: 0 (Success)
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int fd_src, fd_dest, res;
-	char *buffer;
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
-	(void)argc;
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	fd_src = open(argv[1], O_RDONLY);
-	if (fd_src < -1)
-		exit(error_handler(fd_src, argv[1], READ_ERROR, NULL));
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
-	buffer = create_buffer(argv[2]);
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
 
-	umask(0);
-	fd_dest = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fd_dest < -1)
-		exit(error_handler(fd_dest, argv[2], WRITE_ERROR, buffer));
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 
-	do {
-		res = read(fd_src, buffer, BUFFERSIZE);
-		if (res == -1)
-			exit(error_handler(res, argv[1], READ_ERROR, buffer));
-		res = write(fd_dest, buffer, res);
-		if (res == -1)
-			exit(error_handler(res, argv[2], WRITE_ERROR, buffer));
-		fd_dest = open(argv[2], O_RDWR | O_APPEND);
-		if (fd_dest == -1)
-			exit(error_handler(fd_dest, argv[2], WRITE_ERROR, buffer));
-
-	} while (res > 0);
-	close_file(fd_src);
-	close_file(fd_dest);
-	free(buffer);
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 	return (0);
 }
